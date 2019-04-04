668f45ce3830d7cc7fdfb6b14c1e592ad630aed4
Kresimir Sutalo <kresimir095@gmail.com>
2019-04-04 21:30:20 +0200

    .

--- oznacavanjeReporta.js
+++ oznacavanjeReporta.js
@@ -1,9 +1,11 @@
+const limit =500; 
+
 var reports=$(".reportInfo");
 
 for (let i = 0; i < reports.length; i++) {
     const element = reports[i];
     // console.log(element.alt.split("/")[0]);
-    if (element.alt.split("/")[0] > 500) {
+    if (element.alt.split("/")[0] > limit) {
         element.style.backgroundColor  = "red";
     }
 }
