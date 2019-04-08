7d157f88dc835f878c5c03e982e2903d6f1906db
Kresimir Sutalo <kresimir095@gmail.com>
2019-04-08 19:52:31 +0200

    radiiiii

--- slanjeListe.js
+++ slanjeListe.js
@@ -4,19 +4,21 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://tx3.balkans.travian.com/build.php?gid=16&tt=99
+// @match        https://tx3.balkans.travian.com/*
 // @grant        none
 // ==/UserScript==
-
 
 (function() {
     'use strict';
 
     // Your code here...
-
+    if(document.URL.indexOf("tt=99")>-1 && document.URL.toString()!="https://tx3.balkans.travian.com/build.php?tt=99&id=39")
+       {
+        window.location.replace("https://tx3.balkans.travian.com/build.php?tt=99&id=39");
+    }
 
 var nasumican=Math.floor(Math.random() * 60000);
-var periodSlanja=12000 + nasumican; //svako koliko ce se slati lista u milisekundama
+var periodSlanja=120000 + nasumican;
 
 setTimeout(() => {
     var liste = $(".markAll.check");
