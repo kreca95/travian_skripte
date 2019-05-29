dfa75bc092236dae6a7737b4e5b77abfd2fafad1
Kresimir Sutalo <kresimir095@gmail.com>
2019-05-29 13:43:17 +0200

    ispravkeeeeeeeeeee2

--- trgovci.js
+++ trgovci.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Slanje farm liste
+// @name         slanje trgovaca
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -8,39 +8,39 @@
 // @grant        none
 // ==/UserScript==
 (function() {
-if (window.location.href.indexOf("id=31")>-1) {
-    var merchants = $(".send_res tbody tr .max a");
-$(".carry").append("<br><a class='slanje'>00 SIROTILJA</a>"); $(".carry").append("<br><a class='slanje'>01 O SU</a>"); $(".carry").append("<br><a class='slanje'>02 NA</a>");
-
-
-
-const clickMerch = (merch, clicks) => {
-    for (var i = 0; i < 20; i++) {
-        for (var j = 0; j < clicks; j++) {
-            merch[j].click();
+    if (window.location.href.indexOf("id=31")>-1) {
+        var merchants = $(".send_res tbody tr .max a");
+    $(".carry").append("<br><a class='slanje'>00 SIROTILJA</a>"); $(".carry").append("<br><a class='slanje'>01 O SU</a>"); $(".carry").append("<br><a class='slanje'>02 NA</a>");
+    
+    
+    
+    const clickMerch = (merch, clicks) => {
+        for (var i = 0; i < 20; i++) {
+            for (var j = 0; j < clicks; j++) {
+                merch[j].click();
+            }
         }
     }
-}
-//checks for wheat minus
-if ($(".alert").length > 0) {
-    clickMerch(merchants, 3);
-}
-else {
-    clickMerch(merchants, 4);
-}
-
-$(".slanje").click((e) => {
-    // console.log(e.target.innerText);
-    $("#enterVillageName").val(e.target.innerText);
-
-    $("#enabledButton").click();
-    setTimeout(() => {
-        $("#enabledButton").click();
-    }, 1000);
+    //checks for wheat minus
+    if ($(".alert").length > 0) {
+        clickMerch(merchants, 3);
+    }
+    else {
+        clickMerch(merchants, 4);
+    }
     
-
-
-})
-}
-
-})();
+    $(".slanje").click((e) => {
+        // console.log(e.target.innerText);
+        $("#enterVillageName").val(e.target.innerText);
+    
+        $("#enabledButton").click();
+        setTimeout(() => {
+            $("#enabledButton").click();
+        }, 1000);
+    
+    
+    
+    })
+    }
+    
+    })();
