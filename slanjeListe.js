e9b35e2fb5b7fe43ff22cd4409bf363926ed22ac
Kresimir Sutalo <kresimir095@gmail.com>
2019-06-03 01:54:46 +0200

    prije slanja liste provjeri crvene reporte

--- slanjeListe.js
+++ slanjeListe.js
@@ -16,7 +16,28 @@
        {
         window.location.replace("https://tx3.balkans.travian.com/build.php?tt=99&id=39");
     }
+    var napad=$(".iReport3").closest("td").next("td").children();
 
+    for (let i = 0; i < napad.length; i++) {
+        const element = napad[i];
+        setTimeout(() => {
+            element.click();
+            if ($("#t10").val()!=691) {
+                $("#t10").val(691);
+                $("#save").click();
+                $(".overlay").click();
+                setTimeout(() => {
+                    $(".overlay").click();
+                }, 500);
+
+            }
+            else{
+                $(".overlay").click()
+            }
+
+        }, 1500*i);
+
+    }
 var nasumican=Math.floor(Math.random() * 60000);
 var periodSlanja=60000 + nasumican;
 var dateObj = new Date(Date.now() + periodSlanja);
