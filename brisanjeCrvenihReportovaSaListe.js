6ea0c219f5d30552d291732a9a4b0921d4bd708b
Kresimir Sutalo <kresimir095@gmail.com>
2019-06-03 01:35:55 +0200

    bisanje crvenih reportova2

--- brisanjeCrvenihReportovaSaListe.js
+++ brisanjeCrvenihReportovaSaListe.js
@@ -1,18 +1,28 @@
-var napad=$(".iReport3").closest("td").next("td").children();
 
-// setTimeout(() => {
-    
-//     }
-// }, 500);
+// ==UserScript==
+// @name         popravak farm liste
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://tx3.balkans.travian.com/*
+// @grant        none
+// ==/UserScript==
 
-for (let i = 0; i < napad.length; i++) {
-    const element = napad[i];
-    setTimeout(() => {
-		element.click();
-        if ($("#t10").val()!=691) {
-            $("#t10").val(691);
-            $("#save").click();
-            $("#dialogCancelButton").click();
-        }
-    }, 1500*i);
-}
+(function() {
+    'use strict';
+    var napad=$(".iReport3").closest("td").next("td").children();
+
+    for (let i = 0; i < napad.length; i++) {
+        const element = napad[i];
+        setTimeout(() => {
+            element.click();
+            if ($("#t10").val()!=691) {
+                $("#t10").val(691);
+                $("#save").click();
+                $("#dialogCancelButton").click();
+            }
+        }, 1500*i);
+
+    }
+})();
