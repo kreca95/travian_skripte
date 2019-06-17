c090b6210ca5562777b06e54f425b5fd23c134c0
Kresimir Sutalo <kresimir095@gmail.com>
2019-06-17 23:59:49 +0200

    o su stari azijatu

--- slanjeListe.js
+++ slanjeListe.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://tx3.balkans.travian.com/*
+// @match        https://tx3.travian.com.my/*
 // @grant        none
 // ==/UserScript==
 
@@ -12,9 +12,9 @@
     'use strict';
 
     // Your code here...
-    if(document.URL.indexOf("tt=99")>-1 && document.URL.toString()!="https://tx3.balkans.travian.com/build.php?tt=99&id=39")
+    if(document.URL.indexOf("tt=99")>-1 && document.URL.toString()!="https://tx3.travian.com.my/build.php?tt=99&id=39")
        {
-        window.location.replace("https://tx3.balkans.travian.com/build.php?tt=99&id=39");
+        window.location.replace("https://tx3.travian.com.my/build.php?tt=99&id=39");
     }
     var napad=$(".iReport3").closest("td").next("td").children();
 
@@ -39,7 +39,7 @@
 
     }
 var nasumican=Math.floor(Math.random() * 60000);
-var periodSlanja=60000 + nasumican;
+var periodSlanja=6000 + nasumican;
 var dateObj = new Date(Date.now() + periodSlanja);
 document.getElementById("content").append(dateObj);
     var liste = $(".markAll.check");
