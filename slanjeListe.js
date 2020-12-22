d063796290393755f4c8dc9724a0a17c18224d52
kreca95 <43502736+kreca95@users.noreply.github.com>
2020-12-22 14:33:37 +0100

    -- 2021 update
    -- 2021 update

--- slanjeListe.js
+++ slanjeListe.js
@@ -1,54 +1,46 @@
 // ==UserScript==
-// @name         Slanje farm liste
+// @name         Slanje farm liste 2021
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://tx3.travian.com.my/*
+// @match        https://tx3.balkans.travian.com/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
 
-    // Your code here...
-    if(document.URL.indexOf("tt=99")>-1 && document.URL.toString()!="https://tx3.travian.com.my/build.php?tt=99&id=39")
-       {
-        window.location.replace("https://tx3.travian.com.my/build.php?tt=99&id=39");
-    }
-    var napad=$(".iReport3").closest("td").next("td").children();
+console.log("pocetak");
 
-    for (let i = 0; i < napad.length; i++) {
-        const element = napad[i];
-        setTimeout(() => {
-            element.click();
-            if ($("#t10").val()!=691) {
-                $("#t10").val(691);
-                $("#save").click();
-                $(".overlay").click();
-                setTimeout(() => {
-                    $(".overlay").click();
-                }, 500);
-
-            }
-            else{
-                $(".overlay").click()
-            }
-
-        }, 1500*i);
-
-    }
-var nasumican=Math.floor(Math.random() * 60000);
-var periodSlanja=6000 + nasumican;
-var dateObj = new Date(Date.now() + periodSlanja);
-document.getElementById("content").append(dateObj);
-    var liste = $(".markAll.check");
-    var random = Math.floor((Math.random() * liste.length));
-    liste[random].click()
-setTimeout(() => {
-
-    $("button:contains('Započeti pljačku')")[random].click();
+if (document.URL.indexOf("tt=99") > -1) {
+    Init();
 }
-,periodSlanja);
+else{
+    console.log("replace");
+}
 
-})();
+function Init() {
+    'use strict';
+    var minTime = 1600;
+    var maxTime = 5000;
+    var actualTime = Math.floor(Math.random() * maxTime) + minTime;
+    var htmlItem = $(".expandCollapse");
+    var listLength = htmlItem.length;
+    var listNumber = Math.floor(Math.random() * listLength);
+    setTimeout(() => {
+        htmlItem[listNumber].click();
+        console.log("klikanje liste");
+
+    }, 1000);
+
+    setTimeout(() => {
+        $(".markAll.check")[listNumber].click();
+        console.log("klikanje checkboxa");
+    }, actualTime);
+
+    setTimeout(() => {
+        $('button[type="button"][value="Početak"]')[listNumber].click();
+        console.log("SLANJE");
+        location.reload();
+    }, actualTime + 1000);
+
+}
